7d81bdd4db11ecb72252302da8c4c5fd3395094c
Dom Porada <dom@dom.engineering>
2026-01-26 13:02:29 +0100

    Refactor workflows (#3)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		coverage: {
-			thresholds: {
-				branches: 100,
-				functions: 100,
-				statements: 100,
-			},
-		},
 		typecheck: {
 			enabled: true,
 		},
