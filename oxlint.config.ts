f62f3633e092dc0d0331a9dc623d4bc6ec1f6e7f
Dom Porada <dom@dom.engineering>
2026-03-30 07:51:22 +0200

    Update dependencies

--- oxlint.config.ts
+++
@@ -1,6 +0,0 @@
-import { getOxlintConfigs } from '@standard-config/eslint/utilities';
-import { defineConfig } from '@standard-config/oxlint';
-
-const { configBase } = getOxlintConfigs();
-
-export default defineConfig(configBase);
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineOxlintConfig } from '@standard-config/eslint/utilities';
+import { defineConfig } from 'vite-plus';
+
+export default defineConfig({
+	lint: defineOxlintConfig(),
+	pack: {
+		deps: {
+			skipNodeModulesBundle: true,
+		},
+		dts: {
+			sourcemap: true,
+		},
+		entry: 'src/index.ts',
+		failOnWarn: true,
+		publint: true,
+		sourcemap: true,
+	},
+	staged: {
+		'*': [
+			() => 'pnpm install --ignore-scripts',
+			'prettier --ignore-unknown --write',
+			() => 'pnpm prepack',
+		],
+	},
+});
